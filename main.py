d1ce903081e2fdc669772f668f46eec10ca9e005
Aliona <aderkach1991@gmail.com>
2023-12-30 18:53:10 +0200

    square "г" is done

---
+++ main.py
@@ -0,0 +1,22 @@
+x = int(input("Введіть непарне число:"))
+y = int(input("Введіть те саме число ще раз:"))
+#Щоб вийшла рівнобічна фігура спочатку має бути квадрат
+
+i = j = 0
+while i < y:
+    j = 0
+    while j < x:
+        if j <= (x/2):
+            if i + j >= y - 1:
+                print(f'[{i},{j}]', end=' ')
+            else:
+                print(f'     ', end=' ')
+        else:
+            if i > (y/2):
+                if i >= j:
+                    print(f'[{i},{j}]', end=' ')
+            else:
+                print(f'     ', end=' ')
+        j += 1
+    print()
+    i += 1
