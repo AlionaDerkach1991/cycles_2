77f92564aa85b21381e6d7f4cd36659b60ca1d4f
Aliona <aderkach1991@gmail.com>
2023-12-30 20:54:57 +0200

    square "к" is done

---
+++ main.py
@@ -0,0 +1,21 @@
+x = int(input("Введіть число:"))
+y = int(input("Введіть те саме число ще раз:"))
+#Щоб вийшла рівнобічна фігура спочатку має бути квадрат
+
+i = j = 0
+while i < y:
+    j = 0
+    while j < x:
+        if i <= int(y/2):
+            if i + j >= x - 1:
+                print(f'[{i},{j}]', end=' ')
+            else:
+                print(f'     ', end=' ')
+        else:
+            if i + j >= y - 1:
+                print(f'[{i},{j}]', end=' ')
+            else:
+                print(f'     ', end=' ')
+        j += 1
+    print()
+    i += 1
